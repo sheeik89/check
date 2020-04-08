2e4a146fe88c20bc7b5cc251ed9f611fac541cd1
MrNolan <sheeik89@yahoo.in>
2020-04-08 23:56:31 +0530

    local class path set2

--- src/Program.bat
+++ src/Program.bat
@@ -1,4 +1,4 @@
 @echo off
 javac src/Sample1.java
-set classpath = C:\Program Files (x86)\Jenkins\workspace\Git Prac\src\
+SET CLASSPATH = .;C:\Program Files (x86)\Jenkins\workspace\Git Prac\src
 java Sample1
