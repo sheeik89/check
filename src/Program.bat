e7937d7512e775fcf1ed6bdea911b57e5a362aee
MrNolan <sheeik89@yahoo.in>
2020-04-08 12:51:50 +0530

    Minor changes

---
+++ src/Program.bat
@@ -0,0 +1,5 @@
+@echo off
+javac Sample1.java
+java Sample1
+javac Sample2.java
+java Sample2
--- src/Sample2.java
+++ src/Sample2.java
@@ -1,4 +1,7 @@
 
 public class Sample2 {
+	public static void main(String[] args) {
+		System.out.println("Inside Sample 2");
+	}
 
 }
