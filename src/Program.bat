4dc6b5f2cc3fe20dcae8b5e92626387baafd49e4
MrNolan <sheeik89@yahoo.in>
2020-04-09 00:40:21 +0530

    local class path set9

--- src/Program.bat
+++ src/Program.bat
@@ -1,4 +1,4 @@
 @echo off
-javac -"C:\Program Files (x86)\Jenkins\workspace\src" -d"C:\Program Files (x86)\Jenkins\workspace\src" Sample1.java
+javac "-C:\Program Files (x86)\Jenkins\workspace\src" "-d C:\Program Files (x86)\Jenkins\workspace\src" Sample1.java
 java Sample1
 
