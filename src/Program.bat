ee082b8ecf08184911d801a290c02281df8991b2
MrNolan <sheeik89@yahoo.in>
2020-04-09 00:33:27 +0530

    local class path set7

--- src/Program.bat
+++ src/Program.bat
@@ -1,5 +1,5 @@
 @echo off
-javac C:\Program Files (x86)\Jenkins\workspace\src\Sample1.java
+javac "C:\Program Files (x86)\Jenkins\workspace\src\Sample1.java"
 SET CLASSPATH = .;C:\Program Files (x86)\Jenkins\workspace\src
 java Sample1
 
