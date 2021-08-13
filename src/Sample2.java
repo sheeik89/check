43ddea483d77bf77e3d82b8b6fcf4502605d28cc
MrNolan <sheeik89@yahoo.in>
2021-08-13 19:51:02 +0530

    f2

--- src/Sample2.java
+++ src/Sample2.java
@@ -2,6 +2,7 @@ package src;
 public class Sample2 {
 	public static void main(String[] args) {
 		System.out.println("Inside Sample 2");
+		System.out.println("I made this commit from Branch 4");
 	}
 
 }
