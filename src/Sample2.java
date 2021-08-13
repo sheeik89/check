a4fccaaf9ae758f36504f0e2beb51c29d166ad1e
MrNolan <sheeik89@yahoo.in>
2021-08-13 23:13:51 +0530

    four-five

--- src/Sample2.java
+++ src/Sample2.java
@@ -3,8 +3,8 @@ public class Sample2 {
 	public static void main(String[] args) {
 		System.out.println("Inside Sample 2");
 		System.out.println("I made this commit from Branch 4");
-		System.out.println("I made this commit from Branch 4 - Again 1");
-		System.out.println("I made this commit from Branch 4 - Again 2");
+		System.out.println("I made this commit from Branch 5");
+		System.out.println("I made this commit from Branch 5 - new");
 	}
 
 }
